a2f2a7b05555f560b9e214b9481a23ca54692f4a
Soumya Snigdha Kundu <soumyawork15@gmail.com>
2024-05-11 14:22:59 +0100

    Update dice.py

--- nnunetv2/training/loss/dice.py
+++ nnunetv2/training/loss/dice.py
@@ -131,8 +131,15 @@ def get_tp_fp_fn_tn(net_output, gt, axes=None, mask=None, square=False):
     :param square: if True then fp, tp and fn will be squared before summation
     :return:
     """
+    print()
+    print('This is within the function')
+    print()
+    
     if axes is None:
         axes = tuple(range(2, net_output.ndim))
+        print('This is axes')
+        print(axes)
+        print()
 
     with torch.no_grad():
         if net_output.ndim != gt.ndim:
@@ -144,6 +151,14 @@ def get_tp_fp_fn_tn(net_output, gt, axes=None, mask=None, square=False):
         else:
             y_onehot = torch.zeros(net_output.shape, device=net_output.device)
             y_onehot.scatter_(1, gt.long(), 1)
+
+    print('This is net_output')
+    print(net_output.shape)
+    print()
+
+    print('This is y_onehot')
+    print(y_onehout.shape)
+    print()
 
     tp = net_output * y_onehot
     fp = net_output * (1 - y_onehot)
